18c2121ab7c07f1956f62b6e493961ebf088bd9a
shalkoff <neoProgrammer@bk.ru>
2016-10-14 23:02:51 +0300

    Во фрагменты "В город" и "С города" добавлено обновление времени при свайпе вниз, появляется круглая вращающая стрелка(SwipeRefreshLayout)

--- app/src/main/java/ru/bus/raspisanie/shalk_off/raspisanie_bus/fragments/SgorodaFragment.java
+++ app/src/main/java/ru/bus/raspisanie/shalk_off/raspisanie_bus/fragments/SgorodaFragment.java
@@ -3,8 +3,10 @@ package ru.bus.raspisanie.shalk_off.raspisanie_bus.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,21 +23,25 @@ import ru.bus.raspisanie.shalk_off.raspisanie_bus.R;
 /**
  * Created by shalk_off on 06.10.2016.
  */
-public class SgorodaFragment extends Fragment {
+public class SgorodaFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
     ArrayList<Raspisanie> raspList = new ArrayList<>();
     ListAdapterRasp listAdapterRasp;
     ListView listView;
     String sGoroda;
     String[] sGorodaArray;
     Caclulate caclulate;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    View view;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_s_goroda, container, false);
+        view = inflater.inflate(R.layout.fragment_s_goroda, container, false);
         caclulate = new Caclulate();
         Intent intent = getActivity().getIntent();
         sGoroda = intent.getStringExtra("sGoroda");
         sGorodaArray = caclulate.parseAvto(sGoroda);
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.sgoroda_swipe_refresh_layout);
+       mSwipeRefreshLayout.setOnRefreshListener(this);
         // создаем адаптер
         getRaspisanie();
         listAdapterRasp = new ListAdapterRasp(getContext(), raspList);
@@ -52,8 +58,17 @@ public class SgorodaFragment extends Fragment {
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        listView.setAdapter(null);
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+                listView.setAdapter(null);
+                raspList.clear();
+                getRaspisanie();
+                listAdapterRasp = new ListAdapterRasp(getContext(), raspList);
+                listView.setAdapter(listAdapterRasp);
+            }
+        }, 400);
     }
 }
--- app/src/main/java/ru/bus/raspisanie/shalk_off/raspisanie_bus/fragments/VgorodFragment.java
+++ app/src/main/java/ru/bus/raspisanie/shalk_off/raspisanie_bus/fragments/VgorodFragment.java
@@ -2,8 +2,10 @@ package ru.bus.raspisanie.shalk_off.raspisanie_bus.fragments;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,25 +21,29 @@ import ru.bus.raspisanie.shalk_off.raspisanie_bus.R;
 /**
  * Created by shalk_off on 06.10.2016.
  */
-public class VgorodFragment extends Fragment {
+public class VgorodFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
     ArrayList<Raspisanie> raspList = new ArrayList<>();
     ListAdapterRasp listAdapterRasp;
     String vGorod;
     String[] vGorodArray;
     Caclulate caclulate;
+    SwipeRefreshLayout mSwipeRefreshLayout;
+    View view;
+    ListView listView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_v_gorod, container, false);
+        view = inflater.inflate(R.layout.fragment_v_gorod, container, false);
         caclulate = new Caclulate();
         Intent intent = getActivity().getIntent();
         vGorod = intent.getStringExtra("vGorod");
         vGorodArray = caclulate.parseAvto(vGorod);
-
+        mSwipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.vgorod_swipe_refresh_layout);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
         // создаем адаптер
         getRaspisanie();
         listAdapterRasp = new ListAdapterRasp(getContext(), raspList);
-        ListView listView = (ListView) view.findViewById(R.id.list1);
+        listView = (ListView) view.findViewById(R.id.list1);
         listView.setAdapter(listAdapterRasp);
         return view;
     }
@@ -46,5 +52,20 @@ public class VgorodFragment extends Fragment {
             raspList.add(new Raspisanie(vGorodArray[i], caclulate.status(vGorodArray[i]), caclulate. image()));
         }
 
+    }
+
+    @Override
+    public void onRefresh() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(false);
+                listView.setAdapter(null);
+                raspList.clear();
+                getRaspisanie();
+                listAdapterRasp = new ListAdapterRasp(getContext(), raspList);
+                listView.setAdapter(listAdapterRasp);
+            }
+        }, 400);
     }
 }
